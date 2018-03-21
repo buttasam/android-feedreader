48fbcf50a3af6f4d371c42f48a54ce6a5fbd3bcb
Samuel Butta <samuel@butta.cz>
2018-03-21 15:46:21 +0100

    Add top bar menus

--- app/src/main/java/buttasam/cvut/cz/android_feedreader/activity/ArticleDetailActivity.java
+++ app/src/main/java/buttasam/cvut/cz/android_feedreader/activity/ArticleDetailActivity.java
@@ -2,6 +2,8 @@ package buttasam.cvut.cz.android_feedreader.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import buttasam.cvut.cz.android_feedreader.R;
@@ -18,7 +20,18 @@ public class ArticleDetailActivity extends AppCompatActivity {
         System.out.println(id);
 
         // enable back button
+        android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_article_detail, menu);
+
+        return true;
     }
 
     @Override
--- app/src/main/java/buttasam/cvut/cz/android_feedreader/activity/ArticleListActivity.java
+++ app/src/main/java/buttasam/cvut/cz/android_feedreader/activity/ArticleListActivity.java
@@ -2,6 +2,8 @@ package buttasam.cvut.cz.android_feedreader.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import buttasam.cvut.cz.android_feedreader.R;
 
@@ -11,6 +13,17 @@ public class ArticleListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article_list);
+
+        android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_article_list, menu);
+
+        return true;
+    }
 }
